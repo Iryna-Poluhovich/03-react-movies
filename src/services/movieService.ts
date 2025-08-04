c66bd8023b1ff5de1a66bf2f751c6ae805346d28
POLIUKHOVYCH IRYNA <iryna0425@gmail.com>
2025-08-04 11:14:32 +0300

    finish upgrade

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://api.themoviedb.org/3/search/movie";
 const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 interface FetchMoviesResponse {
